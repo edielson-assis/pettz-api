e42c1947edbb7f40cc9f3f2f6286f645c156a4b3
Edielson Assis <edielsonnice@hotmail.com>
2024-09-10 02:22:44 -0300

    docs: documented the endpoints in the UserController class with Swagger

--- src/main/java/br/com/pettz/controller/UserController.java
+++ src/main/java/br/com/pettz/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.pettz.controller.swagger.UserControllerSwagger;
 import br.com.pettz.dtos.request.UserRequest;
 import br.com.pettz.dtos.response.UserResponse;
 import br.com.pettz.models.User;
@@ -24,7 +25,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RestController
 @RequestMapping(value = "/api/v1/auth", produces = "application/json")
-public class UserController {
+public class UserController implements UserControllerSwagger {
 
     private final UserService service;
     private final AuthenticationManager manager;
@@ -38,6 +39,7 @@ public class UserController {
         return ResponseEntity.created(uri).body(user);
     }
 
+    @Transactional
     @PostMapping(path = "/login")
     public ResponseEntity<TokenJWT> login(@RequestBody @Valid UserRequest user) {
         var authenticationToken = new UsernamePasswordAuthenticationToken(user.email(), user.password());
--- src/main/java/br/com/pettz/dtos/response/UserResponse.java
+++ src/main/java/br/com/pettz/dtos/response/UserResponse.java
@@ -2,7 +2,14 @@ package br.com.pettz.dtos.response;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record UserResponse(UUID idUser, String email) {}
+@Schema(name = "Register User Response DTO", description = "DTO for login and registering a new user")
+public record UserResponse(
+    
+    UUID idUser, 
+    
+    @Schema(description = "Email of the user.", example = "maria@example.com")
+    String email) {}
