237a89b365df75d9c1a2fd33a9c0b0b86ae65276
Edielson Assis <edielsonnice@hotmail.com>
2024-09-09 21:36:39 -0300

    feat: created java project

---
+++ src/main/java/br/com/pettz/PettzApplication.java
@@ -0,0 +1,13 @@
+package br.com.pettz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettzApplication.class, args);
+	}
+
+}
